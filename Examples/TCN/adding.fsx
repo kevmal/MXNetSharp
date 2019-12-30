7ab4f1ed279f872821fa0ee7ca59865d7e030e3f
Kevin Malenfant <kevin@xaai.ch>
2019-12-30 16:52:29 -0700

    docs

--- Examples/TCN/adding.fsx
+++ Examples/TCN/adding.fsx
@@ -1,4 +1,13 @@
-﻿#load "../load.fsx"
+﻿(********************************************************************************************************
+ * F# implementation of "The Adding Problem" from
+ * "An Empirical Evaluation of Generic Convolutional and Recurrent Networks for Sequence Modeling"
+ * (Shaojie Bai and J. Zico Kolter and Vladlen Koltun)
+ * arXiv:1803.01271
+ * https://arxiv.org/abs/1803.01271
+ * PyTorch implementation: https://github.com/locuslab/TCN
+ ********************************************************************************************************)
+
+#load "../load.fsx"
 open Load
 
 open System
@@ -10,13 +19,12 @@ open MXNetSharp.SymbolOperators
 open MXNetSharp.Interop
 open MXNetSharp.PrimitiveOperators
 
-let seed = 393939
-MXLib.randomSeed seed
-let rng = Random(seed)
 
+// ******************************************** Parameters **********************************************
+let seed = 393939
 let batchSize = 32
 let seqLength = 400
-let epochs = 30
+let epochs = 10
 let lr = 4e-3
 let ksize = 7
 let levels = 8
@@ -25,8 +33,12 @@ let inputChannels = 2
 let nclasses = 1
     
 let context = if MXLib.getGpuCount() > 0 then GPU 0 else CPU 0
+
+MXLib.randomSeed seed
+let rng = Random(seed)
 let withName name (symbol : #Symbol) = symbol.Name <- name; symbol
 
+// ******************************************** Model **********************************************
 let weightNormalization name =
     let g = Variable(name + "_g")
     let v = Variable(name + "_v")
@@ -36,7 +48,6 @@ let weightNormalization name =
     w :> Symbol    
 
 let temporalBlock name inCount outputCount kernelSize stride dilation padding dropout (x : Symbol) = 
-    printfn "%A" (kernelSize, stride, padding, dilation)
     let conv1 = Convolution(x, 
                             weightNormalization (name + "_conv1_weight"),
                             Variable("_conv1_bias"),
@@ -47,7 +58,7 @@ let temporalBlock name inCount outputCount kernelSize stride dilation padding dr
                             pad = [padding],  
                             noBias = false,
                             Name = name + "_conv1")
-    let conv1Sliced = Slice((conv1), [None;None;None], [None; None; Some -padding], [None;None;None])
+    let conv1Sliced = conv1.[*,*,.. -padding] 
     let relu1 = Relu(conv1Sliced, Name = name + "_relu1")
     let dropout1 = Dropout(relu1, dropout, DropoutMode.Training)
     let conv2 = Convolution(dropout1,
@@ -60,7 +71,7 @@ let temporalBlock name inCount outputCount kernelSize stride dilation padding dr
                             pad = [padding],  
                             noBias = false,
                             Name = name + "_conv2")
-    let conv2Sliced = Slice((conv2), [None;None;None], [None; None; Some -padding], [None;None;None])
+    let conv2Sliced = conv2.[*,*,.. -padding] 
     let relu2 = Relu(conv2Sliced, Name = name + "_relu2")
     let dropout2 = Dropout(relu2, dropout, DropoutMode.Training)
     let final = dropout2 :> Symbol
@@ -73,7 +84,87 @@ let temporalBlock name inCount outputCount kernelSize stride dilation padding dr
     relu :> Symbol
 
 
+let make numInputs outCount numChannels kernelSize dropout x = 
+    (((0,numInputs),x :> Symbol),numChannels)
+    ||> Seq.fold
+        (fun ((i, lastN), last : Symbol) outCount ->
+            let d = pown 2 i
+            let padding = (kernelSize - 1) * d
+            let y = temporalBlock (sprintf "L%d" i) lastN outCount kernelSize 1 d padding dropout last
+            (i + 1, outCount), y
+        )
+    |> snd
+    .>> SwapAxis(dim1 = 1, dim2 = 2)
+    .>> FullyConnected(numHidden = outCount, Name = "final_fc", flatten = false)
+    .>> SwapAxis(dim1 = 1, dim2 = 2)
 
+let x = Input("xIn", shape = [0; inputChannels; seqLength])
+let tcn = make inputChannels nclasses (Array.create levels nhidden) ksize 0.0 x
+let label = Input("y", shape = [0; 1])
+
+let z = 
+    let tcn = SwapAxis(tcn,0,2)
+    let tcn = SwapAxis(tcn,1,2)
+    SequenceLast(tcn)
+
+let loss = (label - z) .>> Square() .>> Mean() .>> MakeLoss()
+
+// ******************************************** Optimizer **********************************************
+type AdamOptimizer(e : Executor, lr, ?beta1, ?beta2) = 
+    let beta1 = defaultArg beta1 0.9
+    let beta2 = defaultArg beta2 0.999
+    let epsilon = 1e-8
+    let mutable count = 1
+    let d = Dictionary<string, NDArray*NDArray>()
+    let lu (s : String) (a : NDArray) =
+        let scc,v = d.TryGetValue(s)
+        if scc then 
+            v
+        else
+            let v = MX.ZerosLike(a),MX.ZerosLike(a)
+            d.[s] <- v
+            v
+    member x.Update() =
+        let t = count //self._index_update_count[index]
+        let coef1 = 1.0 - Math.Pow(beta1, double t)
+        let coef2 = 1.0 - Math.Pow(beta2, double t)
+        let lr = lr*(sqrt(coef2)/coef1)
+        count <- count + 1
+        e.Bindings
+        |> Seq.iter
+            (fun a ->
+                match a with 
+                | ArgBinding ({Name = name; OpReqType = Some WriteTo; Grad = Some grad; NDArray = Some weight}) -> 
+                    let m,v = lu name grad
+                    MX.AdamUpdate([weight], weight, grad, m, v, lr, beta1, beta2, epsilon)
+                | _ -> ()
+            )
+
+
+// ******************************************** Initilization **********************************************
+let bm2 = 
+    let bm = 
+        loss.Bindings
+        |> Bindings.batchSize batchSize 
+        |> Bindings.inferShapes loss
+        |> Bindings.init 
+            (fun a shape ->  
+                let fanin = 
+                    if shape.Length = 3 then    
+                        double a.Shape.Value.[1]*double a.Shape.Value.[2]
+                    elif shape.Length = 1 then 
+                        double a.Shape.Value.[0]
+                    else
+                        double a.Shape.Value.[1]
+                let alpha = sqrt 5.0
+                let gain = sqrt(2.0 / (1.0 + alpha*alpha))
+                let stdv = sqrt(3.0) * (gain) / sqrt(fanin)
+                MX.RandomUniformNDArray(context, -stdv, stdv, a.Shape.Value)
+            )
+    bm
+
+
+// ******************************************** Data gen **********************************************
 let genSample count = 
     let x = Array.CreateInstance(typeof<float32>, count, 2, seqLength)
     let ra = ResizeArray()
@@ -98,94 +189,16 @@ let genSample count =
     CPU(0).CopyFrom(x), CPU(0).CopyFrom(ra.ToArray(), [count; 1])
 
 
+printfn "Generate train set"
 let trainSet = genSample 50000
+printfn "Generate test set"
 let testSet = genSample 1000
 
 
-let make numInputs outCount numChannels kernelSize dropout x = 
-    (((0,numInputs),x :> Symbol),numChannels)
-    ||> Seq.fold
-        (fun ((i, lastN), last : Symbol) outCount ->
-            let d = pown 2 i
-            let padding = (kernelSize - 1) * d
-            let y = temporalBlock (sprintf "L%d" i) lastN outCount kernelSize 1 d padding dropout last
-            (i + 1, outCount), y
-        )
-    |> snd
-    .>> SwapAxis(dim1 = 1, dim2 = 2)
-    .>> FullyConnected(numHidden = outCount, Name = "final_fc", flatten = false)
-    .>> SwapAxis(dim1 = 1, dim2 = 2)
-
-
-let x = Input("xIn", shape = [0; inputChannels; seqLength])
-let tcn = make inputChannels nclasses (Array.create levels nhidden) ksize 0.0 x
-let label = Input("y", shape = [0; 1])
-
-let z = 
-    let tcn = SwapAxis(tcn,0,2)
-    let tcn = SwapAxis(tcn,1,2)
-    SequenceLast(tcn)
-
-let loss = (label - z) .>> Square() .>> Mean() .>> MakeLoss()
-
-
-let mutable beta1 = 0.9
-let mutable beta2 = 0.999
-let mutable count = 1
-let mutable clr = lr
-let preOpt() = 
-    let t = count //self._index_update_count[index]
-    let coef1 = 1.0 - Math.Pow(beta1, double t)
-    let coef2 = 1.0 - Math.Pow(beta2, double t)
-    clr <- lr*(sqrt(coef2)/coef1)
-    count <- count + 1
-let optUpdate (e : Executor) = 
-    let mutable epsilon = 1e-8
-    let d = Dictionary<string, NDArray*NDArray>()
-    let lu = 
-        fun (s : String) (a : NDArray) ->
-            let scc,v = d.TryGetValue(s)
-            if scc then 
-                v
-            else
-                let v = MX.ZerosLike(a),MX.ZerosLike(a)
-                d.[s] <- v
-                v
-    fun () -> 
-        e.Bindings
-        |> Seq.iter
-            (fun a ->
-                match a with 
-                | ArgBinding ({Name = name; OpReqType = Some WriteTo; Grad = Some grad; NDArray = Some weight}) -> 
-                    let m,v = lu name grad
-                    MX.AdamUpdate([weight], weight, grad, m, v, clr, beta1, beta2, epsilon)
-                | _ -> ()
-            )
-
-
-let bm2 = 
-    let bm = 
-        loss.Bindings
-        |> Bindings.batchSize batchSize 
-        |> Bindings.inferShapes loss
-        |> Bindings.init 
-            (fun a shape ->  
-                let fanin = 
-                    if shape.Length = 3 then    
-                        double a.Shape.Value.[1]*double a.Shape.Value.[2]
-                    elif shape.Length = 1 then 
-                        double a.Shape.Value.[0]
-                    else
-                        double a.Shape.Value.[1]
-                let alpha = sqrt 5.0
-                let gain = sqrt(2.0 / (1.0 + alpha*alpha))
-                let stdv = sqrt(3.0) * (gain) / sqrt(fanin)
-                MX.RandomUniformNDArray(context, -stdv, stdv, a.Shape.Value)
-            )
-    bm
+// ******************************************** Training **********************************************
 
 let exe = loss.Bind(context, bm2)
-let opt = optUpdate exe
+let opt = AdamOptimizer(exe,lr)
 
 let testBatch (ix : int []) = 
     let allX,allY = testSet
@@ -227,8 +240,7 @@ let trainBatch (ix : int []) =
     ys.CopyTo(bm2.NDArray(label))
     exe.Forward(true)
     exe.Backward()
-    preOpt()
-    opt()
+    opt.Update()
     exe.Outputs.[0].ToFloat32Scalar()
 
 let trainEpoch () = 
@@ -239,6 +251,7 @@ let trainEpoch () =
     |> Array.average
 
 
+printfn "Training for %d epochs..." epochs
 let sw = Stopwatch()
 sw.Start()
 for i = 1 to epochs do 
